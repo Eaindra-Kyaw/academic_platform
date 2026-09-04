9df0dc95f359bcd01b296dfcfe57421871b76436
Eaindra <eaindrakyaw887@gmail.com>
2026-09-04 23:52:49 +0630

    Fixed PHP and config to prevent array bug on Railway

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => 'https://academicplatform-production.up.railway.app',
 
     'asset_url' => env('ASSET_URL', null),
 
